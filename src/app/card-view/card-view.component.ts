3d301855e7b5b0b7c59b7d61c74242315aac707f
wangy <wangy@transip.nl>
2023-02-20 13:48:19 +0100

    udpate cardView

--- src/app/card-view/card-view.component.ts
+++ src/app/card-view/card-view.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-card-view',
   templateUrl: './card-view.component.html',
   styleUrls: ['./card-view.component.css']
 })
-export class CardViewComponent {
+export class CardViewComponent implements AfterViewInit{
   constructor(
+    private elementRef: ElementRef
   ){}
+ 
+  ngAfterViewInit() {
+    // Have access to window.document object and set styles
+    // https://stackoverflow.com/questions/46670795/how-to-change-whole-page-background-color-in-angular
+    this.elementRef.nativeElement.ownerDocument
+        .body.style.backgroundImage = "url(./../../assets/icons/gradient-center-light-red-light-blue.png)";
+}
   }
