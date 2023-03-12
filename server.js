8f2667b7eb47a37d923ca351148c695d198131c1
wangy <wangy@transip.nl>
2023-03-12 18:12:18 +0100

    update

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/pingpong-admin'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/pingpong-admin/index.html'));});
 app.listen(process.env.PORT || 8080);
