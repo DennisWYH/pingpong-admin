cff45c710e863565cec9559715278fc02eb3f663
wangy <wangy@transip.nl>
2023-03-14 08:35:55 +0100

    update

--- src/app/add-sentence/add-sentence.component.ts
+++ src/app/add-sentence/add-sentence.component.ts
@@ -55,7 +55,7 @@ export class AddSentenceComponent {
     // setting the content type to anything other than application/x-www-form-urlencoded, 
     // multipart/form-data, or text/plain will trigger the browser to send a preflight OPTIONS request to the server.
     // https://stackoverflow.com/questions/25727306/request-header-field-access-control-allow-headers-is-not-allowed-by-access-contr
-    this.http.post('http://localhost:8080/add-sentence', body, )
+    this.http.post('https://pingpong-fun.herokuapp.com/add-sentence', body, )
     .subscribe(
       response => {
         this.response = response;
